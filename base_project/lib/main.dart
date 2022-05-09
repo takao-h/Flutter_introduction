9fd151be37dc831aec79f97a3125b4cc9e3b4c49
takao-h <takao.hayashi@zozo.com>
2022-05-09 11:26:08 +0900

    changed

--- base_project/lib/main.dart
+++ base_project/lib/main.dart
@@ -1,60 +1,28 @@
-import 'dart:js';
-
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: Page1(),
+    ),
+  );
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({ Key? key }) : super(key: key);
-
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return const Divider();
-
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-    }
-        return Text(
-          _suggestions[index].asPascalCase,
-          style: _biggerFont,
-        );
-    },
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('welcome to Flutter'),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(_createRoute());
+          },
+          child: const Text('Go!'),
         ),
-        body: const Center(
-          child: RandomWords(),
-        )
-      )
+      ),
     );
   }
 }
@@ -67,17 +35,13 @@ Route _createRoute() {
       const end = Offset.zero;
       const curve = Curves.ease;
 
-      final tween = Tween(begin: begin, end: end);
-      final curvedAnimation = CurvedAnimation(
-        parent: animation,
-        curve: curve,
-      );
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
       return SlideTransition(
-        position: tween.animate(curvedAnimation),
+        position: animation.drive(tween),
         child: child,
       );
-      },
+    },
   );
 }
 
@@ -94,5 +58,3 @@ class Page2 extends StatelessWidget {
     );
   }
 }
-
-
